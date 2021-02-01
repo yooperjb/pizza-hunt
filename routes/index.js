65575ce62d75a90a50deaa0b0698033f5d575e4b
Pedal <jmb181@humboldt.edu>
2021-01-31 20:14:04 -0800

    initial setup of mongo model

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
+// Import all of the API routes from /api/index.js (no need for index.js though since it's implied)
+const apiRoutes = require('./api');
 const htmlRoutes = require('./html/html-routes');
 
+router.use('/api', apiRoutes);
 router.use('/', htmlRoutes);
 
 router.use((req, res) => {
